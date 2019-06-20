dfda6414a39db2cabe576deeadecc51aa3603321
Forrest Trepte <forrest@microsoft.com>
2019-06-20 13:00:16 -0700

    code cleanup/rename

---
+++ Assets/MixedRealityToolkit.Examples/Demos/HandTracking/Script/OpenBrowser.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
+{
+    public class OpenBrowser : MonoBehaviour
+    {
+        public void Launch(string url)
+        {
+            Debug.Log($"BrowserIntegration: Launching browser at {url}");
+
+#if WINDOWS_UWP
+        UnityEngine.WSA.Application.InvokeOnUIThread(async () =>
+        {
+            var uri = new System.Uri(url);
+            bool result = await Windows.System.Launcher.LaunchUriAsync(uri);
+            if (!result)
+            {
+                Debug.LogError("Browser failed to launch.");
+            }
+        }, false);
+#else
+            Application.OpenURL(url);
+#endif
+        }
+    }
+}
